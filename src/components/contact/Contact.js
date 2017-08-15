d392657f1c22770bd8cf9378820b0bb7b603116d
Ivan <ijaimes2872@gmail.com>
2017-08-15 14:48:49 -0500

    Home p changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,6 +7,8 @@ const Contact = () => (
         <div className="contact--links">
             <a className="contact--link" href="mailto:ijaimes2872@gmail.com">Email</a> 
             <br / >  
+            <a className="contact--link" href="https://github.com/ij2827">Github</a>
+            <br / >  
             <a className="contact--link" href="https://soundcloud.com/elesion">Soundcloud</a>
         </div>
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,9 +5,13 @@ const Home = () => (
     <div className="main-content">
         <h2>Home</h2>
         <div className="divider"></div>
-        <p>This is the home page</p>
-
-
+        <p>Hello!</p>
+        <p>My name is Ivan Jaimes and I am a recent Graduate from Stephen F. Austin State University with a Bachelors of Music with emphases in Sound Recording Technology. During my time in University, I was involved as Recording Crew Manager for the School of Music and Sound Engineer for the School of Theatres Voice-Over class.
+ <br /></p>
+        <p>Along with a love for music, I have a strong passion with computers. My love for technology started when I was around 14. My little cousin forgot his password to his computer and had no way in logging in. Having a basic knowledge of Linux, I remembered that there was a Linux Distro called Backtrack full of hacking tools. With the help of Backtrack and a lot of patience, I was able to unlock his computer by changing his password. This had me hooked and eventually led to me learning how to program.
+ <br /></p>
+        <p>During my senior year of college, I decided that I wanted to work with computers. So I started to focus my time learning Java and Python. Then I moved on to Web Development in which I learned HTML, CSS, Javascript, Jquery, and Bootstrap. After learning the basics of Front-End Web Development, I moved towards the backend by learning NodeJs, Express, MongoDB, and React Js. 
+ <br /></p>
         
         
     </div>
