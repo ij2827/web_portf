8b4be125f5a490c0fb60554dd3658583de7c5568
Ivan <ijaimes2872@gmail.com>
2017-08-22 00:35:14 -0500

    add Refix...

--- docs/bundle.js
+++ docs/bundle.js
@@ -26916,7 +26916,7 @@ var componentName = function (_Component) {
                     case "+":
                         this.setState({
                             displayValue: tempValue ? String(parseFloat(displayValue) + parseFloat(tempValue)) : String(parseFloat(prevValue) + parseFloat(displayValue)),
-                            tempValue: tempValue ? tempValue : prevValue
+                            tempValue: tempValue ? tempValue : displayValue
                         });
                         break;
                     case "-":
